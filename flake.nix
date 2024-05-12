7dc892c6047a9fde69cee8b711495897dae7cd88
Christopher Fredén <me@icetan.org>
2024-05-12 17:08:38 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { self, nixpkgs, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      # packages = forEachSystem (system: {
+      #   devenv-up = self.devShells.${system}.default.config.procfileScript;
+      # });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = pkgs.mkShell {
+              buildInputs = [
+                (pkgs.ruby.withPackages (ps: with ps; [ jekyll ]))
+                pkgs.instaloader
+              ];
+              shellHook = ''
+                echo hello
+              '';
+            };
+          });
+    };
+}
