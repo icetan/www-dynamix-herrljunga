675120ddc9db942e448321ff7b883ce4f20f97f2
Christopher Fredén <me@icetan.org>
2025-09-23 12:28:10 +0200

    Restore instagram dir before pull

--- bin/update-commit-push.sh
+++ bin/update-commit-push.sh
@@ -5,11 +5,16 @@ set -eo pipefail
   set -x
   nix develop . --command bash -c '
 set -xe
+
+INSTA_DIR=./instagram
+
+git restore --source=HEAD --staged --worktree -- "$INSTA_DIR"
+git clean -df "$INSTA_DIR"
 git pull
 ./bin/mk-config.sh
 ./bin/update-instagram.sh
 ./bin/gen-posts.sh
-git add ./instagram
+git add "$INSTA_DIR"
 if git commit -m "Created by bin/update-commit-push.sh from $(uname -n)"; then
   git push
 fi
